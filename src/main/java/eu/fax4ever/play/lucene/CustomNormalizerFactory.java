315318c82f2b65d8accb625cde49b1e925dc7ba6
Yoann Rodière <yoann@hibernate.org>
2019-04-15 17:21:44 +0200

    Demonstrate how .normalize works correctly with CustomAnalyzer

--- src/main/java/eu/fax4ever/play/lucene/CustomNormalizerFactory.java
+++ src/main/java/eu/fax4ever/play/lucene/CustomNormalizerFactory.java
@@ -6,8 +6,13 @@ import java.util.Collections;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.core.KeywordTokenizerFactory;
 import org.apache.lucene.analysis.core.LowerCaseFilterFactory;
+import org.apache.lucene.analysis.custom.CustomAnalyzer;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.util.ClasspathResourceLoader;
+import org.apache.lucene.analysis.util.ResourceLoader;
+import org.apache.lucene.util.Version;
 
 public class CustomNormalizerFactory {
 
@@ -19,6 +24,17 @@ public class CustomNormalizerFactory {
 	public static Analyzer createLowerCaseNormalizerAlt() {
 		LowerCaseFilterFactory lowerCaseFilterFactory = new LowerCaseFilterFactory( Collections.emptyMap() );
 		return new CustomNormalizerAlt( lowerCaseFilterFactory );
+	}
+
+	public static Analyzer createLowerCaseNormalizerCustomAnalyzer() throws IOException {
+		// TODO in HSearch, use the HibernateSearchResourceLoader; see org.hibernate.search.backend.lucene.analysis.impl.LuceneAnalysisComponentFactory#LuceneAnalysisComponentFactory
+		ResourceLoader resourceLoader = new ClasspathResourceLoader( CustomNormalizerFactory.class );
+		return CustomAnalyzer.builder( resourceLoader )
+				// TODO in HSearch, use the configured Lucene version; see org.hibernate.search.backend.lucene.analysis.impl.LuceneAnalysisComponentFactory#LuceneAnalysisComponentFactory
+				.withDefaultMatchVersion( Version.LATEST )
+				.withTokenizer( KeywordTokenizerFactory.class )
+				.addTokenFilter( LowerCaseFilterFactory.class )
+				.build();
 	}
 
 	/**
